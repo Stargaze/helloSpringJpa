a0d65ffc4f165ab4e3331aea9fa225fbf456342d
Stargaze <molmul99@gmail.com>
2026-04-17 22:47:40 +0900

    feat: DuplicateCategoryException 카테고리 커스텀 예외처리 구현

--- src/main/java/kr/ac/hansung/cse/exception/DuplicateCategoryException.java
+++ src/main/java/kr/ac/hansung/cse/exception/DuplicateCategoryException.java
@@ -1,4 +1,9 @@
 package kr.ac.hansung.cse.exception;
 
-public class DuplicateCategoryException {
+// 카테고리 명 중복 예외처리
+public class DuplicateCategoryException extends RuntimeException {
+
+    public DuplicateCategoryException(String name) {
+        super("이미 존재하는 카테고리입니다: " + name);
+    }
 }
